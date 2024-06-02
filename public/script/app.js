e2169f35631eb65d8144736722255e2dc5954fa3
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2024-06-02 19:52:26 +0530

    add styles to form

--- public/script/app.js
+++ public/script/app.js
@@ -10,31 +10,37 @@ const placeholder = document.querySelector(".placeholder");
 
 form.addEventListener("submit", async (eve) => {
   eve.preventDefault();
-  if (
-    cityInput.value !== "" &&
-    (latitude.value !== "" || longitude.value !== "")
-  ) {
-    alert("Please enter either city or latitude & longitude");
-    return;
-  }
-  if (cityInput.value !== "") {
-    const data = await sendData(
-      `http://localhost:3300/weather?info=city&cityname=${cityInput.value}`
-    );
-    display(data);
-    cityInput.value = "";
-    return;
-  }
-  if (latitude.value !== "" && longitude.value !== "") {
-    const data = await sendData(
-      `http://localhost:3300/weather?info=coordinates&lat=${latitude.value}&long=${longitude.value}`
-    );
-    display(data);
-    latitude.value = "";
-    longitude.value = "";
-    return;
-  } else {
-    alert("Please enter both latitude and longitude");
+  try {
+    if (
+      cityInput.value !== "" &&
+      (latitude.value !== "" || longitude.value !== "")
+    ) {
+      alert("Please enter either city or latitude & longitude");
+      return;
+    }
+    if (cityInput.value !== "") {
+      const data = await sendData(
+        `http://localhost:3300/weather?info=city&cityname=${cityInput.value}`
+      );
+      display(data);
+      cityInput.value = "";
+      return;
+    }
+    if (latitude.value !== "" && longitude.value !== "") {
+      const data = await sendData(
+        `http://localhost:3300/weather?info=coordinates&lat=${latitude.value}&long=${longitude.value}`
+      );
+      display(data);
+      latitude.value = "";
+      longitude.value = "";
+      return;
+    } else {
+      alert("Please enter both latitude and longitude");
+    }
+  } catch (err) {
+    display({
+      message: "Unexpected error",
+    });
   }
 });
 
@@ -49,7 +55,7 @@ const display = function (data) {
   if (Object.hasOwn(data, "message")) {
     const h4 = document.createElement("h4");
     h4.className = "error";
-    h4.innerText = `Location not found`;
+    h4.innerText = `${data.message}`;
     placeholder.appendChild(h4);
     return;
   }
